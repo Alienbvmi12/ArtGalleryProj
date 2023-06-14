ca9be176051e854c7b7597e4f214a3a514525054
alien_bvmi12 <rayfathur2006@gmail.com>
2023-06-14 15:57:23 +0700

    third commit

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
 {
@@ -10,5 +13,26 @@ class RegisterController extends Controller
         return view('register.index', [
             'title' => 'Register'
         ]);
+    }
+
+    public function register(Request $request){
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required|max:255|email:dns|unique:users,email',
+            'username' => 'required|max:255|min:3|unique:users,username',
+            'password' => 'required|max:255'
+        ]);
+
+        $validatedData['password'] = Hash::make($validatedData['password']);
+
+        $validatedData['user_level'] = 'user';
+        $validatedData['ban_data_id'] = null;
+
+        User::create($validatedData);
+
+        $request->session()->flash('success', 'Berhasil register!! silahkan login');
+
+        return redirect('/login');
+
     }
 }
